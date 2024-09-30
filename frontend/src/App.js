d0357fb7d43eb50b0f2a9c8b939aa713e84a6428
Parshva Bhadra <parshva.bhadra@sevn.ai>
2024-09-30 11:38:23 +0000

    Refactor: Add text editor component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { Provider } from 'react-redux';
 import store from './store/store';
+import Editor from './components/textEditor/Editor';
 
 function App() {
   return (
     <Provider store={store}>
       {/* ... existing components ... */}
+      <Editor />
     </Provider>
   );
 }
