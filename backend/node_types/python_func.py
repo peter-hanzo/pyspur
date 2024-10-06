78b44791c74ac595963d05acb0bf92283b73f652
Srijan Patel <mail.srijanpatel@gmail.com>
2024-10-06 15:48:37 +0000

    feat: Update PythonFuncNodeType to support dynamic input and output schemas

--- backend/node_types/python_func.py
+++ backend/node_types/python_func.py
@@ -1,18 +1,20 @@
-from typing import Any, Dict
-from pydantic import BaseModel
-from .base import BaseNodeType
+from typing import Any, Dict, Type
+from pydantic import BaseModel, create_model
+from .base import BaseNodeType, DynamicSchemaValueType
 
 
 class PythonFuncNodeConfig(BaseModel):
     code: str  # The Python code to execute
+    input_schema: Dict[str, DynamicSchemaValueType]  # The input schema
+    output_schema: Dict[str, DynamicSchemaValueType]  # The output schema
 
 
 class PythonFuncNodeInput(BaseModel):
-    input_data: Dict[str, Any]
+    pass
 
 
 class PythonFuncNodeOutput(BaseModel):
-    output_data: Any
+    pass
 
 
 class PythonFuncNodeType(
@@ -26,15 +28,34 @@ class PythonFuncNodeType(
 
     def __init__(self, config: PythonFuncNodeConfig) -> None:
         self.config = config
+        input_schema = config.input_schema
+        input_schema = {k: self._get_python_type(v) for k, v in input_schema.items()}
+        input_schema = {k: (v, ...) for k, v in input_schema.items()}
+        self.input_model = create_model(
+            "PythonFuncNodeInput",
+            **input_schema,  # type: ignore
+            __base__=BaseModel,
+        )
+        output_schema = config.output_schema
+        output_schema = {k: self._get_python_type(v) for k, v in output_schema.items()}
+        output_schema = {k: (v, ...) for k, v in output_schema.items()}
+        self.output_model = create_model(
+            "PythonFuncNodeOutput",
+            **output_schema,  # type: ignore
+            __base__=BaseModel,
+        )
+        self.InputType = self.input_model
+        self.OutputType = self.output_model
 
     async def __call__(self, input_data: PythonFuncNodeInput) -> PythonFuncNodeOutput:
         # Prepare the execution environment
         exec_globals = {}
-        exec_locals = {"input_data": input_data.input_data, "output_data": None}
+        print("input_data", input_data.model_dump())
+        exec_locals = {"input_data": input_data.model_dump()}
 
         # Execute the user-defined code
         exec(self.config.code, exec_globals, exec_locals)
 
         # Retrieve the output data
         output_data = exec_locals.get("output_data")
-        return PythonFuncNodeOutput(output_data=output_data)
+        return self.output_model(**output_data)
