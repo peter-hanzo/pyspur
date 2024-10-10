1d6235cdcfdecaf3894aa489856d89ec28141837
Jean Kaddour <jean.kaddour@icloud.com>
2024-10-10 19:29:04 +0100

    add missing types

--- examples/arc/prompting.py
+++ examples/arc/prompting.py
@@ -6,10 +6,11 @@ from functools import cache
 import os
 import io
 import contextlib
-from typing import Any, Callable, Optional, TypeVar
+from typing import Any, Callable, Optional, TypeVar, Union, Literal
 import base64
 import asyncio
 import itertools
+from enum import Enum, auto
 
 from scipy.ndimage import label, generate_binary_structure
 import attrs
@@ -47,6 +48,83 @@ from examples.arc.reasoning_examples import (
     reasoning_labeled_change_prompt_alt_color_add_swap,
 )
 from feature_engineering import DisplayArgs
+
+
+class ContextLengthExceeded(Exception):
+    original_exception: Exception
+
+    def __init__(self, original_exception: Exception):
+        super().__init__(str(original_exception))
+        self.original_exception = original_exception
+
+
+@attrs.frozen
+class TokenUsage:
+    input_tokens: int
+    output_tokens: int
+
+    @property
+    def total_tokens(self):
+        return self.input_tokens + self.output_tokens
+
+
+class StopReason(Enum):
+    MAX_TOKENS = auto()
+    STOP_SEQUENCE = auto()
+    WEIRD = auto()
+
+    @classmethod
+    def factory(cls, stop_reason: Union[str, StopReason]) -> StopReason:
+        """
+        Parses the openai and anthropic stop reasons into a StopReason enum.
+        """
+        if isinstance(stop_reason, StopReason):
+            return stop_reason
+
+        stop_reason = stop_reason.lower()
+        if stop_reason in ["max_tokens", "length"]:
+            return cls.MAX_TOKENS
+        elif stop_reason in ["stop_sequence", "stop"]:
+            return cls.STOP_SEQUENCE
+        elif stop_reason in ["weird"]:
+            return cls.WEIRD
+        raise ValueError(f"Invalid stop reason: {stop_reason}")
+
+    @classmethod
+    def from_anthropic(
+        cls, stop_reason: Literal["end_turn", "max_tokens", "stop_sequence"]
+    ) -> StopReason:
+        if stop_reason in ["max_tokens"]:
+            return cls.MAX_TOKENS
+        elif stop_reason in ["stop_sequence", "end_turn"]:
+            # We could have a different one for stop_sequence vs end_turn
+            return cls.STOP_SEQUENCE
+
+        raise ValueError(f"Invalid stop reason: {stop_reason}")
+
+    def __repr__(self):
+        return self.name
+
+
+@attrs.frozen
+class LLMResponse:
+    model_id: str
+    completion: str
+    stop_reason: StopReason = attrs.field(converter=StopReason.factory)
+    cost: float
+    logprobs: Optional[list[dict[str, float]]] = None
+    token_usage: Optional[TokenUsage] = None
+
+    def as_dict(self):
+        res = attrs.asdict(self)
+        res["stop_reason"] = self.stop_reason.name
+        return res
+
+    @classmethod
+    def from_dict(cls, d: dict[str, Any]):
+        token_usage = op.map(d.get("token_usage"), lambda x: TokenUsage(**x))
+        # d["stop_reason"] = StopReason[d["stop_reason"]]
+        return cls(**{**d, "token_usage": token_usage})
 
 
 def normalize_ordering(items: list[LLMResponse]) -> list[LLMResponse]:
