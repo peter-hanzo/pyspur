64f2c12eac938a6da127b2c024e0e5437b0c418a
Srijan Patel <mail.srijanpatel@gmail.com>
2024-10-22 15:02:44 +0000

    refactor: Update main.py to use NodeFactory for retrieving node types and their schemas

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 from ..execution.workflow_executor_dask import WorkflowExecutorDask
 from ..execution.node_executor import NodeExecutor
-from ..nodes import node_registry
+from ..nodes.factory import NodeFactory
 from fastapi import FastAPI
 from typing import List, Dict, Any
 from ..schemas.workflow import WorkflowNode, Workflow
@@ -22,26 +22,37 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/node_types/", response_model=List[Dict[str, Any]])
-async def get_node_types() -> List[Dict[str, Any]]:
+@app.get("/node_types/")
+async def get_node_types() -> Dict[str, List[Dict[str, Any]]]:
     """
     Returns the schemas for all available node types.
     """
     # get the schemas for each node class
-    node_schemas: List[Dict[str, Any]] = []
-    for node_class in node_registry.values():
-        config_schema = node_class.config_model
-        input_schema = node_class.input_model
-        output_schema = node_class.output_model
-        node_schemas.append(
-            {
-                "name": node_class.__name__,
-                "config": config_schema.model_json_schema(),
-                "input": input_schema.model_json_schema(),
-                "output": output_schema.model_json_schema(),
+    node_groups = NodeFactory.get_all_node_types()
+
+    response: Dict[str, List[Dict[str, Any]]] = {}
+    for group_name, node_types in node_groups.items():
+        node_schemas: List[Dict[str, Any]] = []
+        for node_type in node_types:
+            node_class = node_type.node_class
+            try:
+                input_schema = node_class.input_model.model_json_schema()
+            except AttributeError:
+                input_schema = {}
+            try:
+                output_schema = node_class.output_model.model_json_schema()
+            except AttributeError:
+                output_schema = {}
+            node_schema: Dict[str, Any] = {
+                "name": node_type.node_type_name,
+                "input": input_schema,
+                "output": output_schema,
+                "config": node_class.config_model.model_json_schema(),
             }
-        )
-    return node_schemas
+            node_schemas.append(node_schema)
+        response[group_name] = node_schemas
+
+    return response
 
 
 @app.post("/run_node/")
