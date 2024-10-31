e6c8c0d4620972dc8a059b4c1b6a6735606a4e1a
parshva-bhadra <parshva.bhadra@sevn.ai>
2024-10-31 17:56:33 +0530

    minor change

--- frontend/src/components/textEditor/Editor.jsx
+++ frontend/src/components/textEditor/Editor.jsx
@@ -4,10 +4,9 @@ import Underline from '@tiptap/extension-underline';
 import { Color } from '@tiptap/extension-color';
 import TextStyle from '@tiptap/extension-text-style';
 import ListItem from '@tiptap/extension-list-item';
-import { useEffect } from 'react';
 
 const Editor = (content, setContent, isEditable) => {
-    const editor = useEditor({
+    return useEditor({
         extensions: [
             Color.configure({ types: [TextStyle.name, ListItem.name] }),
             TextStyle.configure({ types: [ListItem.name] }),
@@ -23,19 +22,13 @@ const Editor = (content, setContent, isEditable) => {
                 class: `flex flex-col px-4 py-3 justify-start border-b border-x border-gray-700 items-start w-full gap-3 font-medium pt-4 rounded-b-md outline-none ${isEditable ? "" : "rounded-md border-t"}`,
             },
         },
+        onUpdate: ({ editor }) => {
+            setContent(editor.getHTML());
+        },
         editable: isEditable,
         autofocus: 'end',
         immediatelyRender: false,
     });
-
-    // Update the editor content when the `content` prop changes
-    useEffect(() => {
-        if (editor && content !== editor.getHTML()) {
-            setContent(editor.getHTML());
-        }
-    }, [content, editor]);
-
-    return editor;
 };
 
 export default Editor;
