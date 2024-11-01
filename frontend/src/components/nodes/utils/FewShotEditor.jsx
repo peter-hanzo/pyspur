ffdd0fc64b40c0be042154453a4c060494fb1036
Jean Kaddour <jean.kaddour@icloud.com>
2024-11-01 17:17:42 +0000

    clean up

--- frontend/src/components/nodes/utils/FewShotEditor.jsx
+++ frontend/src/components/nodes/utils/FewShotEditor.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import TextEditor from '../../../textEditor/TextEditor';
-import { updateNodeData } from '../../../../store/flowSlice';
+import TextEditor from '../../textEditor/TextEditor';
+import { updateNodeData } from '../../../store/flowSlice';
 import Tabs from './Tabs';
 import { Button } from "@nextui-org/react";
 
