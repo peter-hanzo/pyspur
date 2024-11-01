47962779854db75fd3d530efff94648c9b41f410
Jean Kaddour <jean.kaddour@icloud.com>
2024-11-01 21:56:31 +0000

    add redux-persist to make redux store persistent across reloads

--- frontend/src/pages/workflow.jsx
+++ frontend/src/pages/workflow.jsx
@@ -1,11 +1,16 @@
 import FlowCanvas from '../components/canvas/FlowCanvas';
 import Header from '../components/Header';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from '../store/store';
+
 const WorkflowPage = () => {
   return (
-    <div>
-      <Header activePage="workflow" />
-      <FlowCanvas />
-    </div>
+    <PersistGate loading={null} persistor={persistor}>
+      <div>
+        <Header activePage="workflow" />
+        <FlowCanvas />
+      </div>
+    </PersistGate>
   );
 };
 
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import flowReducer from './flowSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['nodes', 'edges'],
+};
+
+const persistedReducer = persistReducer(persistConfig, flowReducer);
 
 const store = configureStore({
   reducer: {
-    flow: flowReducer,
+    flow: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
 });
 
+export const persistor = persistStore(store);
 export default store;
