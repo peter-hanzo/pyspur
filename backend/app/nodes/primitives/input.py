5faac93dd0d915e08922efae87e4b9f76001d8f8
Srijan Patel <mail.srijanpatel@gmail.com>
2024-11-06 20:57:53 +0000

    make input nodes mandatory in workflows

--- backend/app/nodes/primitives/input.py
+++ backend/app/nodes/primitives/input.py
@@ -1,9 +1,13 @@
-from pydantic import BaseModel, create_model
+from typing import Dict
+from pydantic import BaseModel, model_validator
 from ..dynamic_schema import DynamicSchemaNode, DynamicSchemaNodeConfig
 
 
 class InputNodeConfig(DynamicSchemaNodeConfig):
-    pass
+    @model_validator(mode="before")
+    def set_output_schema_same_as_input(cls, data: Dict[str, str]) -> Dict[str, str]:
+        data["output_schema"] = data["input_schema"]
+        return data
 
 
 class InputNodeInput(BaseModel):
@@ -23,14 +27,6 @@ class InputNode(DynamicSchemaNode):
     config_model = InputNodeConfig
     input_model = InputNodeInput
     output_model = InputNodeOutput
-
-    def setup(self) -> None:
-        self.input_model = create_model(
-            "DatasetInputNodeInput",
-            **self.config.input_schema,
-            __base__=BaseModel,
-        )
-        self.output_model = self.input_model
 
     async def run(self, input_data: BaseModel) -> BaseModel:
         return input_data
