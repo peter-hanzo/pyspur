838b2a67c1ba7a25bbfaa2cc232758f79757988f
Srijan Patel <mail.srijanpatel@gmail.com>
2024-11-20 23:12:34 +0000

    fix bug w/ partial runs

--- frontend/src/components/nodes/BaseNode.jsx
+++ frontend/src/components/nodes/BaseNode.jsx
@@ -18,6 +18,7 @@ const BaseNode = ({ isCollapsed, setIsCollapsed, id, data = {}, children, style 
   const [showControls, setShowControls] = useState(false);
   const [isTooltipHovered, setIsTooltipHovered] = useState(false);
   const [editingTitle, setEditingTitle] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
   const dispatch = useDispatch();
 
   // Retrieve the node's position and edges from the Redux store
@@ -25,6 +26,25 @@ const BaseNode = ({ isCollapsed, setIsCollapsed, id, data = {}, children, style 
   const edges = useSelector((state) => state.flow.edges);
   const hoveredNodeId = useSelector((state) => state.flow.hoveredNode);
   const selectedNodeId = useSelector((state) => state.flow.selectedNode);
+
+  const initialInputs = useSelector((state) => {
+    const inputNodeId = state.flow?.nodes.find((node) => node.type === 'InputNode')?.id;
+    let testInputs = state.flow?.testInputs;
+    if (testInputs && Array.isArray(testInputs)) {
+      const { id, ...rest } = testInputs[0];
+      return {[inputNodeId]: rest};
+    }
+  });
+  const availableOutputs = useSelector((state) => {
+    const nodes = state.flow.nodes;
+    const availableOutputs = {};
+    nodes.forEach((node) => {
+      if (node.data && node.data.run) {
+        availableOutputs[node.id] = node.data.run;
+      }
+    });
+    return availableOutputs;
+  });
 
   const { executePartialRun, loading } = usePartialRun();
 
@@ -50,18 +70,26 @@ const BaseNode = ({ isCollapsed, setIsCollapsed, id, data = {}, children, style 
   };
 
   const handlePartialRun = () => {
-    const initialInputs = {
-      "input_node": { "user_message": "Give me geographical conditions of London" }
-    };
-    const partialOutputs = {
-      "input_node": { "user_message": "Hi There!" },
-      "node_1731411247373": { "response": "Hello, How are ya?" }
-    };
+    if (!node) {
+      return;
+    }
+    setIsRunning(true);
     const rerunPredecessors = true;
 
     const workflowId = window.location.pathname.split('/').pop();
 
-    executePartialRun(workflowId, id, initialInputs, partialOutputs, rerunPredecessors);
+    executePartialRun(workflowId, id, initialInputs, availableOutputs, rerunPredecessors).then((result) => {
+      if (result) {
+        Object.entries(result).forEach(([nodeId, output_values]) => {
+          if (output_values) {
+            dispatch(updateNodeData({ id: nodeId, data: { run: { ...node.data.run, ...output_values } } }));
+            dispatch(setSelectedNode({ nodeId }));
+          }
+        });
+      }
+    }).finally(() => {
+      setIsRunning(false);
+    });
   };
 
   const handleDuplicate = () => {
@@ -105,10 +133,11 @@ const BaseNode = ({ isCollapsed, setIsCollapsed, id, data = {}, children, style 
 
   const status = data.run && data.run ? 'completed' : (data.status || 'default').toLowerCase();
 
-  const borderColor = status === 'completed' ? '#4CAF50' :
-    status === 'failed' ? 'red' :
-      status === 'default' ? 'black' :
-        style.borderColor || '#ccc';
+  const borderColor = isRunning ? 'blue' :
+    status === 'completed' ? '#4CAF50' :
+      status === 'failed' ? 'red' :
+        status === 'default' ? 'black' :
+          style.borderColor || '#ccc';
 
   const cardStyle = {
     ...style,
