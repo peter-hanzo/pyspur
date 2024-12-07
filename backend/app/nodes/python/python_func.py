e9cfba3a81722f94e0ef155e49c133d0d0f0d90c
Srijan Patel <mail.srijanpatel@gmail.com>
2024-12-07 07:34:43 +0000

    Refactor PythonFuncNode class for MISO

--- backend/app/nodes/python/python_func.py
+++ backend/app/nodes/python/python_func.py
@@ -2,29 +2,28 @@ from typing import Dict, Any
 
 from pydantic import BaseModel
 
-from ..dynamic_schema import DynamicSchemaNode, DynamicSchemaNodeConfig
+from ..base import BaseNode, BaseNodeConfig, BaseNodeInput, BaseNodeOutput
 
 
-class PythonFuncNodeConfig(DynamicSchemaNodeConfig):
-    input_schema: Dict[str, str] = {}
+class PythonFuncNodeConfig(BaseNodeConfig):
     code: str = "\n".join(
         [
             "# Write your Python code here.",
-            "# All the inputs to the node will be available as local variables.",
+            '# The input data is available as "input" pydantic model.',
             "# The output will be the local variables that are also in the output schema.",
         ]
     )
 
 
-class PythonFuncNodeInput(BaseModel):
+class PythonFuncNodeInput(BaseNodeInput):
     pass
 
 
-class PythonFuncNodeOutput(BaseModel):
+class PythonFuncNodeOutput(BaseNodeOutput):
     pass
 
 
-class PythonFuncNode(DynamicSchemaNode):
+class PythonFuncNode(BaseNode):
     """
     Node type for executing Python code on the input data.
     """
@@ -34,11 +33,14 @@ class PythonFuncNode(DynamicSchemaNode):
     input_model = PythonFuncNodeInput
     output_model = PythonFuncNodeOutput
 
-    async def run(self, input_data: BaseModel) -> BaseModel:
+    def setup(self) -> None:
+        return super().setup()
+
+    async def run(self, input: BaseModel) -> BaseModel:
         # Prepare the execution environment
         exec_globals: Dict[str, Any] = {}
-        print("input_data", input_data.model_dump())
-        exec_locals = input_data.model_dump()
+        exec_locals: Dict[str, Any] = {"input": input}
+        print("input", input.model_dump())
 
         # Execute the user-defined code
         exec(self.config.code, exec_globals, exec_locals)
@@ -50,3 +52,28 @@ class PythonFuncNode(DynamicSchemaNode):
             if key in self.config.output_schema
         }
         return self.output_model.model_validate(output_data)
+
+
+if __name__ == "__main__":
+    from pydantic import BaseModel, create_model
+    import asyncio
+
+    config = PythonFuncNodeConfig(
+        code="\n".join(
+            [
+                "# Write your Python code here.",
+                '# The input data is available as "input" pydantic model.',
+                "# The output will be the local variables that are also in the output schema.",
+                "output = input.A.number ** 2",
+            ]
+        ),
+        output_schema={"output": "int"},
+    )
+    A = create_model(
+        "Input", number=(int, ...), __base__=BaseNodeOutput
+    ).model_validate({"number": 5})
+    input = {"A": A}
+    node = PythonFuncNode(config=config)
+
+    output = asyncio.run(node(input))
+    print(output)
