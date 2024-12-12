11f85e2973b106220bb0bed9269a9b139dbb0e69
Srijan Patel <mail.srijanpatel@gmail.com>
2024-12-12 06:27:23 +0000

    initialise node title with id

--- frontend/src/utils/nodeFactory.ts
+++ frontend/src/utils/nodeFactory.ts
@@ -69,41 +69,22 @@ export const createNode = (
     return null;
   }
 
-  const inputProperties = cloneDeep(nodeType.input?.properties) || {};
-  const outputProperties = cloneDeep(nodeType.output?.properties) || {};
-
   let processedAdditionalData = cloneDeep(additionalData);
-
-  // If the additional data has input/output properties, merge them with the default properties
-  if (additionalData.input?.properties) {
-    processedAdditionalData.input = {
-      ...processedAdditionalData.input,
-      properties: {
-        ...inputProperties,
-        ...additionalData.input.properties,
-      },
-    };
-  }
-
-  if (additionalData.output?.properties) {
-    processedAdditionalData.output = {
-      ...processedAdditionalData.output,
-      properties: {
-        ...outputProperties,
-        ...additionalData.output.properties,
-      },
-    };
-  }
+  let config = cloneDeep(nodeType.config);
+  config = {
+    ...config,
+    title: id,
+  };
 
   const node: Node = {
     id,
     type: nodeType.name,
     position,
     data: {
-      title: nodeType.name === 'InputNode' ? id : nodeType.name,
+      title: id,
       acronym: nodeType.visual_tag.acronym,
       color: nodeType.visual_tag.color,
-      config: cloneDeep(nodeType.config),
+      config: config,
       ...processedAdditionalData,
     },
   };
