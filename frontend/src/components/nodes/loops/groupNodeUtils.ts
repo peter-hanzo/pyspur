c72c5029c78325ade2f5a43df7cbb62da1dcc42a
Srijan Patel <mail.srijanpatel@gmail.com>
2025-01-22 19:09:31 +0000

    feat: expand group node on child node drag

--- frontend/src/components/nodes/loops/groupNodeUtils.ts
+++ frontend/src/components/nodes/loops/groupNodeUtils.ts
@@ -159,6 +159,7 @@ export const onNodeDragStopOverGroupNode = (
             parentId: groupNode.id,
             position,
             extent: 'parent',
+            expandParent: true,
         })
 
         // // Clear group node highlighting
