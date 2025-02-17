b56c7070d4f41596ece1f8da47c5b2a176de867d
Srijan Patel <mail.srijanpatel@gmail.com>
2025-02-17 07:42:33 +0000

    chore: move API app to its own file

--- backend/pyspur/api/main.py
+++ backend/pyspur/api/main.py
@@ -5,26 +5,9 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
-from ..nodes.registry import NodeRegistry
-
-NodeRegistry.discover_nodes()
-
-from ..integrations.google.auth import router as google_auth_router
-from .dataset_management import router as dataset_management_router
-from .evals_management import router as evals_management_router
-from .file_management import router as file_management_router
-from .key_management import router as key_management_router
-from .node_management import router as node_management_router
-from .openai_compatible_api import router as openai_compatible_api_router
-from .output_file_management import router as output_file_management_router
-from .rag_management import router as rag_management_router
-from .run_management import router as run_management_router
-from .template_management import router as template_management_router
-from .workflow_management import router as workflow_management_router
-from .workflow_run import router as workflow_run_router
+from .api_app import api_app
 
 load_dotenv()
-
 
 app = FastAPI()
 
@@ -36,27 +19,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# Create a sub-application for API routes
-api_app = FastAPI(
-    docs_url="/docs",
-    redoc_url="/redoc",
-    title="PySpur API",
-    version="1.0.0",
-)
-api_app.include_router(node_management_router, prefix="/node")
-api_app.include_router(workflow_management_router, prefix="/wf")
-api_app.include_router(workflow_run_router, prefix="/wf")
-api_app.include_router(dataset_management_router, prefix="/ds")
-api_app.include_router(run_management_router, prefix="/run")
-api_app.include_router(output_file_management_router, prefix="/of")
-api_app.include_router(key_management_router, prefix="/env-mgmt")
-api_app.include_router(template_management_router, prefix="/templates")
-api_app.include_router(openai_compatible_api_router, prefix="/api")
-api_app.include_router(evals_management_router, prefix="/evals")
-api_app.include_router(google_auth_router, prefix="/google")
-api_app.include_router(rag_management_router, prefix="/rag")
-api_app.include_router(file_management_router, prefix="/files")
 
 # Mount the API routes under /api
 app.mount("/api", api_app, name="api")
