fb40019bd3f45bd9e8f9c00b7d0b6b427df38630
Jean Kaddour <jean.kaddour@icloud.com>
2025-03-20 17:01:44 +0000

    fix: remove unused import

--- frontend/src/components/slack/index.ts
+++ frontend/src/components/slack/index.ts
@@ -1,6 +1,5 @@
 export { default as SlackSetupGuide } from './SlackSetupGuide'
 export { default as WorkflowAssociationModal } from './WorkflowAssociationModal'
 export { default as SlackAgentWizard } from './SlackAgentWizard'
-export { default as SlackAgentDetail } from './SlackAgentDetail'
 export { default as AgentTokenManager } from './AgentTokenManager'
 export { default as SlackTestConnection } from './SlackTestConnection'
